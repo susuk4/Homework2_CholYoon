c4aeeb698de05f1ef5fc592ad763ef245575b692
susuk4 <susuk4@uw.edu>
2013-04-16 21:08:00 -0700

    Create FactorInt.py

---
+++ FactorInt.py
@@ -0,0 +1,8 @@
+class FactorInt:
+    def __Init__(self, integer):
+        if type(integer) == int:
+            self.n = integer
+        else:
+            
+    denominators = [2,3,5,7]
+    for y in Denominators
