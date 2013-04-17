b9df768e8450a16847028b54111f7c880f5670db
susuk4 <susuk4@uw.edu>
2013-04-16 21:29:48 -0700

    Update FactorInt.py

--- FactorInt.py
+++ FactorInt.py
@@ -2,7 +2,13 @@ class FactorInt:
     def __Init__(self, integer):
         if type(integer) == int:
             self.n = integer
-        else:
+            self.denominator = [7,5,3,2]
+        else: #Exception when integer is not int   
             
-    denominators = [2,3,5,7]
-    for y in Denominators
+    
+    #looping through 2,3,5,7 which are prime numbers between 1 - 10 except 1
+    for de in denominators
+        #every new loop start while loop until input number is not divisible by new loop number
+        #and add divisible string at the front.
+        
+    
